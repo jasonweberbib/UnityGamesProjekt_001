a23c5ca46609cddab6535a1964a36ce6cad0e56b
jasonweberbib <Jason_Elias.Weber@edu.bib.de>
2024-05-13 00:06:41 +0200

    Animations & Rotation
    1. Wizard looks left and right
    2. Animation is a bit better mabey

--- Assets/Script/Wizard.cs
+++ Assets/Script/Wizard.cs
@@ -6,14 +6,31 @@ using UnityEngine.UIElements;
 
 public class Wizard : MonoBehaviour
 {
+    //Player Movment speed
     float movementSpeed = 4.0f;
+
+    //Refrenc to fireballPrefab
     public GameObject fireballPrefab;
+    // Valuse for Fireball Stamina / Firerate
     private float fireRate = 0.7f;
     private float canFire = -1f;
+
+    //
     Vector3 movement = Vector3.zero;
+
+    // Set Value for Fireball Rotation in fireballPrefab
     float fbRotate = 0;
+
+    // Set Value for Wizard Rotation in Wizard
+    //bool PlayerRotate = true;
+
     public Vector3 direction;
     Vector3 lastMovement = Vector3.zero;
+
+    //Vector3 for Rotatior change
+    //Vector3 PRotate = Vector3.Rotate;
+
+    // Animation refrenc
     private Animator animator;
 
     // Cavnvas Statts
@@ -36,13 +53,23 @@ public class Wizard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Set Waking animation as False with no key pressed
         animator.SetBool("Walking", false);
+        animator.SetBool("Attack", false);
+
 
         Vector3 movement = Vector3.zero;
         if (Input.GetKey("d")) 
         {
             //movement += new Vector3(0.01f, 0,0);
             movement += Vector3.right;
+/*
+            if (PlayerRotate == false){
+                Vector3 rotation = new Vector3(0, 180, 0 * Time.deltaTime);
+                transform.Rotate (rotation);
+                PlayerRotate = true;
+            }
+*/
             fbRotate = 0;
             animator.SetBool("Walking", true);
         } 
@@ -50,6 +77,14 @@ public class Wizard : MonoBehaviour
         {
             //movement += new Vector3(-0.01f, 0, 0);
             movement += Vector3.left;
+            //PRotate = Vector3.Rotate(0,180,0);
+/*
+            if (PlayerRotate == true){
+                Vector3 rotation = new Vector3(0, 180, 0 * Time.deltaTime);
+                transform.Rotate (rotation);
+                PlayerRotate = false;
+            }
+*/
             fbRotate = 180;
             animator.SetBool("Walking", true);
         }
@@ -79,8 +114,24 @@ public class Wizard : MonoBehaviour
             GameObject obj = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
             obj.transform.Rotate(0, 0, fbRotate);
             obj.GetComponent<Fireball>().direction = lastMovement;
+            animator.SetBool("Attack", true);
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("Attack", false);
+        }   
 
         transform.position += movement.normalized * Time.deltaTime * movementSpeed;
+
+        // Flip Character
+        if (movement.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (movement.x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
     }
 }
