68a8eade42cd7602a3d71d8d5c185a44ad7219de
jasonweberbib <Jason_Elias.Weber@edu.bib.de>
2024-06-13 15:13:09 +0200

    srsrh
    yhrrnrent

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set;}
+
+    /// <summary>
+    /// MAIN MENU BUTTENS
+    /// </summary>
+
+    public string state = "";
+    public void startbutten()
     {
+        SceneManager.LoadScene("game_01");
+        state = "Game";
+    }
+
+    public void quitbutten()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Back to Main Menu Timer
+    /// </summary>
+    /// 
+    private float timer;
+    private void Update()
+    {
+        timer =+ 1 * Time.deltaTime;
+        if(state == "Game")
+        {
+            if (timer > 1200)
+            {
+                SceneManager.LoadScene("main_menu");
+                state = "Menu";
+                timer = 0;
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void leDestroy()
     {
-        
+        if(Instance != null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
